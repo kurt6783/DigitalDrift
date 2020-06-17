db23edc2d9c11a31229eeed590b2bcf90e1f39df
vagrant <vagrant@homestead>
2020-06-17 08:17:34 +0000

    功能皆已完成

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -12,7 +12,23 @@
         @foreach($weatherElement['time'] as $time)
           起始時間 - {{$time['startTime']}}
           結束時間 - {{$time['endTime']}}
-          {{$time['parameter']['parameterName']}}
+          {{$time['parameter']['parameterName']}} 
+          @switch($weatherElement['elementName'])
+            @case('Wx')
+              {{$time['parameter']['parameterValue']}} 
+              @break
+            @case('PoP')
+              {{$time['parameter']['parameterUnit']}} 
+              @break
+            @case('MinT')
+              {{$time['parameter']['parameterUnit']}} 
+              @break
+            @case('CI')
+              @break
+            @case('MaxT')
+              {{$time['parameter']['parameterUnit']}} 
+              @break
+          @endswitch         
         @endforeach
       @endforeach
     @endforeach
